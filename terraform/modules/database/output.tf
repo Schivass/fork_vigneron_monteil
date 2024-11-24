3254b945f4ffc8f735e410b249b0765b8b3d92fb
schivass <thomas.vigneron@student.junia.com>
2024-11-24 14:45:36 +0100

    Refacto(terraform) : Delete old files

--- terraform/modules/database/output.tf
+++
@@ -1,9 +0,0 @@
-output "server_address" {
-  value = azurerm_postgresql_flexible_server.playground_computing.fqdn
-  description = "Hostname to connect to the database server"
-}
-
-output "port" {
-  value = 5432
-  description = "Port of the database instance"
-}
--- terraform/modules/storage/output.tf
+++
@@ -1,4 +0,0 @@
-output "url" {
-  value = azurerm_storage_account.storage.primary_blob_endpoint
-  description = "Hostname to connect to the storage account"
-}
